3f73133d8c24727401274ff3bee64e9acc0a38c9
RolandoDU <rolandolopez3442@yahoo.com>
2015-08-13 19:42:10 -0700

    ch5

---
+++ ch5/ex5-4.c
@@ -0,0 +1,21 @@
+//  Program to determine if a number is even or odd (Ver. 2)
+
+#include <stdio.h>
+
+int main()
+{
+	int number_to_test, remainder1;
+
+	printf("Enter you number to be tested: ");
+	scanf("%i", &number_to_test);
+
+	remainder1 = number_to_test % 2;
+
+	if (remainder1 == 0)
+		printf("The number is even.\n");
+
+	else
+		printf("The number is odd.\n");
+
+	return 0;
+}
