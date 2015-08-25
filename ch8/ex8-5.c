42765252e192ad602ad5bdb33d61d6070aa1ae43
RolandoDU <rolandolopez3442@yahoo.com>
2015-08-25 13:34:43 -0700

    ch8ex6

--- ch8/ex8-5.c
+++ ch8/ex8-5.c
@@ -11,5 +11,38 @@ struct time
 
 int main(void)
 {
-	struct time timeUpdate(stru)
+	struct time timeUpdate(struct time now);
+	struct time currentTime, nextTime;
+
+	printf("Enter the time (hh:mm:ss): ");
+	scanf("%i:%i:%i", &currentTime.hour,
+						&currentTime.minutes, &currentTime.seconds);
+
+	nextTime = timeUpdate(currentTime);
+
+	printf("Updated time is %.2i:%.2i:%.2i\n", nextTime.hour,
+								nextTime.minutes, nextTime.seconds);
+
+	return 0;
+}
+
+// Function to update the time by one second
+
+struct time timeUpdate(struct time now)
+{
+	++now.seconds;
+
+	if (now.seconds == 60) {
+		now.seconds = 0;
+		++now.minutes;
+
+		if (now.minutes == 60) {
+			now.minutes = 0;
+			++now.hour;
+
+			if (now.hour == 24)
+				now.hour = 0;
+		}
+	}
+	return now;
 }
